0f568da84794388ded080e505b384c22dc86f273
ZoeSamm <98825845+ZoeSamm@users.noreply.github.com>
2022-10-27 18:20:33 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - printf function
  * @format: format.
- * Return: Printed chars.
+ * Return: printed chars
  */
 int _printf(const char *format, ...)
 {
